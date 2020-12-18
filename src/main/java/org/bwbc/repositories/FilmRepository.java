6abada4422a5eb9c037a3447666a5c565611ff58
Nicolas Tarbouriech <65289313+NicolasTarbouriech@users.noreply.github.com>
2020-12-18 12:25:21 +0100

    TP 4 fini, film repository jpql ok

--- src/main/java/org/bwbc/repositories/FilmRepository.java
+++ src/main/java/org/bwbc/repositories/FilmRepository.java
@@ -11,34 +11,34 @@ public interface FilmRepository extends CrudRepository<Film, Long> {
 	/** Recherche tous les films triés par ordre alphabétique
 	 * @return List
 	 */
-	@Query("From Film")
+	@Query("From Film f ORDER BY f.nom ASC ")
 	List<Film> findFilmsTriesParNom();
 
 	/** Recherche tous les films triés par année ascendante
 	 * @return
 	 */
-	@Query("From Film")
+	@Query("From Film f ORDER BY f.annee ASC")
 	List<Film> findFilmsTriesParAnnee();
 
 	/** Recherche un film à partir d'une partie de son nom
 	 * @param nom nom du film
 	 * @return List
 	 */
-	@Query("From Film")
+	@Query("From Film f where f.nom = :nom")
 	List<Film> findFilmsParNom(String nom);
 
 	/** Retourne tous les films parus l'année passée en paramètre et triés par nom
 	 * @param annee année
 	 * @return List
 	 */
-	@Query("From Film")
+	@Query("From Film f where f.annee= :annee")
 	List<Film> findFilmsParAnnee(int annee);
 
 	/** Retourne tous les films pour un genre donné
 	 * @param genre genre recherché
 	 * @return List
 	 */
-	@Query("From Film")
+	@Query("From Film f JOIN f.genres g WHERE g.nom=:genre")
 	List<Film> findFilmsParGenre(String genre);
 	
 	/** Retourne tous les films réalisés par un réalisateur donné entre 2 dates, triés par année
@@ -47,14 +47,14 @@ public interface FilmRepository extends CrudRepository<Film, Long> {
 	 * @param maxAnnee année max
 	 * @return List
 	 */
-	@Query("From Film")
+	@Query("From Film f JOIN f.realisateurs r WHERE r.identite=:identite and f.annee between :minAnnee and :maxAnnee ORDER BY f.annee")
 	List<Film> findFilmsParRealisateurEtAnneesTriesParAnnee(String identite, int minAnnee, int maxAnnee);
 
 	/** Recherche les films ayant un certain nombre de genres.
 	 * @param nbGenres nombre de genres
 	 * @return List
 	 */
-	@Query("From Film")
+	@Query("From Film f WHERE size(f.genres)=:nbGenres")
 	List<Film> findFilmsParNbGenres(int nbGenres);
 	
 	/** Recherche les films dans lesquels les 2 personnes passées en paramètre ont joué.
@@ -62,7 +62,7 @@ public interface FilmRepository extends CrudRepository<Film, Long> {
 	 * @param identite2 identité de la personne 2
 	 * @return List
 	 */
-	@Query("From Film")
+	@Query("SELECT f1 From Film f1, Film f2 JOIN f1.roles r1 JOIN r1.acteur a1 JOIN f2.roles r2 JOIN r2.acteur a2 WHERE a1.identite=:identite1 and a2.identite=:identite2 AND f1.nom=f2.nom")
 	List<Film> findFilmsAvecPlusieursActeurs(String identite1, String identite2);
 
 }
